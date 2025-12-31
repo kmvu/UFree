6054ccc3d3ccc7272be5a023ad7e3ad031d1000e
Khang Vu <kmvu91@gmail.com>
2025-12-31 16:26:03 +0700

    (FEAT) Added configuration for Firebase dependencies

--- UFree/UFreeApp.swift
+++ UFree/UFreeApp.swift
@@ -7,9 +7,24 @@
 
 import SwiftUI
 import SwiftData
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
 
 @main
 struct UFreeApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     // 1. Initialize SwiftData container for persistence
     let container: ModelContainer
     
